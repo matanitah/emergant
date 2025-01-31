3ee0cd6df31e37e629160f0e0c6e599b7e3bc6c9
Matan Itah <matanitah212@gmail.com>
2025-01-31 18:33:58 -0500

    bugfix

--- src/core/food.py
+++ src/core/food.py
@@ -3,6 +3,6 @@ from config.settings import WIDTH, HEIGHT
 class Food:
     def __init__(self):
         self.food_count = random.randint(50, 250)
-        self.size = self.food_units // 25
+        self.size = self.food_count // 25
         self.x = random.randint(50, WIDTH - 50)
         self.y = random.randint(50, HEIGHT - 50)
